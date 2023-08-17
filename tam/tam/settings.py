8c1473401fe772d46d768ad5be66c54451f7b91e
llshahroozll <shahroozakbari80@gmail.comm>
2023-08-17 20:42:07 +0330

    Add logging and logs file, Add Requirements packages for project

--- tam/tam/settings.py
+++ tam/tam/settings.py
@@ -195,63 +195,61 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# LOGGING ={
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     "loggers": {
-#         "django": {
-#             "handlers": ["file", "console", "mail_admins"],
-#             "level": "INFO",
-#         },
-#         "django.server": {
-#             "handlers": ["django.server"],
-#             "level": "INFO",
-#             "propagate": False,
-#         },
-#     },
-#     "handlers": {
-#         "console": {
-#             "level": "INFO",
-#             "filters": ["require_debug_true"],
-#             "class": "logging.StreamHandler",
-#         },
-#         "django.server": {
-#             "level": "INFO",
-#             "class": "logging.StreamHandler",
-#             "formatter": "django.server",
-#         },
-#         "mail_admins": {
-#             "level": "ERROR",
-#             "filters": ["require_debug_false"],
-#             "class": "django.utils.log.AdminEmailHandler",
-#         },
-#         "file":{
-#             "level":"INFO",
-#             "class": "logging.FileHandler",
-#             "filename":"./log/logs.log",
-#             "formatter":"verbose",
-#         }, 
-#     },
-#     "filters": {
-#         "require_debug_false": {
-#             "()": "django.utils.log.RequireDebugFalse",
-#         },
-#         "require_debug_true": {
-#             "()": "django.utils.log.RequireDebugTrue",
-#         },
-#     },
-#     "formatters": {
-#         "django.server": {
-#             "()": "django.utils.log.ServerFormatter",
-#             "format": "[{server_time}] {message}",
-#             "style": "{",
-#         },
-#         "verbose": {
-#             "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
-#             "format": "[{server_time}] {message}",
-#             "style": "{",
-#         },
-#     },
-    
-    
-# }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        "django.server": {
+            "()": "django.utils.log.ServerFormatter",
+            "format": "[{server_time}] {message}",
+            "style": "{",
+        },
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "./log/logs.log",
+            "formatter": "verbose",
+        },
+        "console": {
+            "level": "INFO",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+        },
+        "django.server": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "django.server",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file", "console", "mail_admins"],
+            "level": "INFO",
+        },
+        "django.server": {
+            "handlers": ["file", "django.server"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
