d74e020b6cb2667421608e56b197dce2685934d2
llshahroozll <shahroozakbari80@gmail.comm>
2023-08-29 17:40:18 +0330

    Change Host

--- tam/tam/settings.py
+++ tam/tam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1#x8jrkc3ovp#j6^#^&wfy$g@34=gbstjvk0jcgmjcx2v9w%f&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.101.186']
+ALLOWED_HOSTS = []
 
 
 # Application definition
